7d4fb6c2b78226838ab1e9c35173b10c42ea3abc
Carlos M. de Lucas <carlanlinux@gmail.com>
2020-12-08 17:17:27 +0100

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,115 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {MongoClient} from 'mongodb';
+import * as path from "path";
+
+//Levantar servidor le decimos que ejecute de nuevo el comando cuando detecte algún cambio:
+// npx nodemon --exec npx babel-node src/server.js
+// node start
+//Ejecutar mongo poner: mongo
+
+
+const app = express();
+
+//--> Indicar la ruta de despliegue de la aplicación
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(bodyParser.json());
+
+//Creamos una función que gestiona la conexión con la base de datos y le pasamos como parámetro las operaciones a realizar
+const withDB = async (operations, res) => {
+    try {
+        //Conectamos con la base de datos, hay que pasar siempre las opciones de newURLparser true
+        const client = await MongoClient.connect('mongodb://localhost:27017', {poolSize: 10, bufferMaxEntries: 0, reconnectTries: 5000, useNewUrlParser: true});
+        //Creamos un objeto base de datos con el nombre de la base de datos que necesitamos
+        const db = client.db('proyecto-fp');
+
+        //Ejecutamos las operaciones que nos pasan por parámetros pasando la base de datos.
+        await operations(db);
+
+        //Cerramos la conexión con la base de datos.
+        await client.close();
+    } catch (e) {
+        //Mandamos el mensaje de error
+        await res.status(500).json({message: "Error al conectar con la base de datos", e});
+    }
+}
+
+
+/* TESTEO de endpoints
+app.get("/hello", (req, res) => res.send("Hello!"));
+app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`))
+app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}`));
+*/
+
+//API endpoint para votar artículos, incluímos la palabra async
+app.post('/api/articles/:name/votar', async (req, res) => {
+    const articleName = req.params.name;
+    withDB( async (db) => {
+        //Sacamos los datos de la info del artículo que se pasa en la URL buscandolo en la colección pasándole el nombre
+        // del artículo en objeto tal como está en la bd
+        const articleInfo = await db.collection('articles').findOne({name: articleName});
+        //Hacemo sun update e la base de datos para sumarle 1 a los votos qeu tiene ese artículo
+        await db.collection('articles').updateOne({name: articleName}, {
+            '$set': {
+                votos: articleInfo.votos + 1
+            },
+        });
+        //Sacamos la información actuializada de la base de datos para mandarla en la respuesta
+        const updatedArticleInfo = await db.collection('articles').findOne({name: articleName});
+        //Enviamos la respuesta en formato JSON con los datos actualizados.
+        await res.status(200).json(updatedArticleInfo);
+    }, res)
+});
+
+
+app.get('/api/articles/:name', async (req, res) => {
+
+    //Llamamos a la función de la base de datos y tenemos como parámetro la propia base de datos y la operación que
+    // queremos realizar
+    withDB(async (db) => {
+        const articleName = req.params.name;
+        //Buscamos en al base de datos el artículo que tenga ese nombre
+        const articleInfo = await db.collection('articles').findOne({name: articleName});
+        //Le asignas el número del estado al constuir la respuesta.
+        await res.status(200).json(articleInfo);
+    }, res);
+
+
+})
+
+app.post('/api/articles/:name/comentar', async (req, res) => {
+    //Recogemos el valor del cuerpo de la request y lo asginamos el primero a la constante username y el segundo a text
+    const {username, text} = req.body;
+    //Recogemos el nombre del artículo de la request, los parámetros y el nombre (:name)
+    const articleName = req.params.name;
+
+    withDB( async (db) => {
+        //Vamos al array que tiene la info de los artículos, entramos en el índe que sea el nombre del artículo, nos vamos a los
+        // comentarios y le hacemos un push al array un nuevo objeto que contenga nombre de usuario y el texto
+        const articleInfo = await db.collection('articles').findOne({name: articleName});
+        await db.collection('articles').updateOne({name : articleName}, {
+            '$set' : {
+                comentarios: articleInfo.comentarios.concat({username, text}),
+            },
+        });
+        const updatedArticleInfo = await db.collection('articles').findOne({name: articleName});
+        //enviamos la respuesta si ha ido bien (status 200) con los comentarios del artículo en cuestión
+        await res.status(200).json(updatedArticleInfo);
+    }, res);
+
+});
+
+/*app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});*/
+
+//Aquí le decimos que lo que nos llegue de cualuqiera de las API REST pase por nuestra app
+app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+})
+
+app.listen(5000, () => console.log("Listening on port 5000"));
+
